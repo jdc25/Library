b0b843d4988be90f6b71bb7e613f428028790af1
Jonathan Schofield <jondc21@outlook.com>
2024-11-30 18:38:05 +0000

    Initial commit with basic project structure

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    //the contructor...
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+}
+
+
+addBookToLibrary("Think Like a Programmer: An Introduction to Creative Problem Solving", "V. Anton Spraul", 256, true);
