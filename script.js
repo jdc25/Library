48e8ff0705b7e0c6cf8f5cca84189fb101bd0e4e
Jonathan Schofield <jondc21@outlook.com>
2024-12-01 17:53:23 +0000

    Add functionality to display books and render dynamically

--- script.js
+++ script.js
@@ -13,5 +13,24 @@ function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(newBook);
 }
 
-
+function render() {
+    const libraryBook = document.querySelector("#library");
+    for (let i = 0; i < myLibrary.length; i++) {
+      const book = myLibrary[i]; // Get each book library array
+      const bookEl = document.createElement("div") // Create a new div for the book
+      bookEl.classList.add("book-card"); 
+  
+    bookEl.innerHTML = `
+     <p><strong>Title:</strong> ${book.title}</p>
+     <p><strong>Author:</strong> ${book.author}</p>
+     <p><strong>Pages:</strong> ${book.pages}</p>
+     <p><strong>Status:</strong> ${book.isRead ? "Read" : "Not Read"}</p>
+     `;
+  
+    // Append book element to library container
+    libraryBook.appendChild(bookEl);
+    }
+  }
+  
 addBookToLibrary("Think Like a Programmer: An Introduction to Creative Problem Solving", "V. Anton Spraul", 256, true);
+render();
